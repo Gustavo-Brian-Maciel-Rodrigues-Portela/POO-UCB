c447004a83805c7bba2a695ce0594657c52a9e81
Gustavo-Brian-Maciel-Rodrigues-Portela <133496080+Gustavo-Brian-Maciel-Rodrigues-Portela@users.noreply.github.com>
2023-08-14 19:02:04 -0300

    Add files via upload

---
+++ Atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,35 @@
+package Atividade1;
+import java.util.Scanner;
+
+public class verificaNumeroPrimo {
+
+    public static int verificaNumeroPrimo(Integer n) {
+        if (n <= 1) {
+            return 0;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return 0;
+            }
+        }
+
+        return 1; // Número é primo
+    }
+
+    public static void main(String[] args) {
+        Integer numero = 0; 
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Insira um número: ");
+        numero = scan.nextInt();
+
+        int resultado = verificaNumeroPrimo(numero);
+
+        if (resultado == 0) {
+            System.out.println(numero + " não é primo.");
+        } else {
+            System.out.println(numero + " é primo.");
+        }
+    }
+}
